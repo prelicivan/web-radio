ce8a022e02792384c6471580816072ed82313575
Ivan Prelic <ivanprelicp@gmail.com>
2024-12-26 16:55:18 +0100

    url changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    API_NOW_PLAYING_URL: "https://51f062b117e47bcf48d973f858b80ae2.loophole.site" + "/api/nowplaying", // port 80
-    API_RADIO_STREAM_URL: "https://99aaaf1a35192948e57f2a120364d4b5.loophole.site" + "/radio.mp3" // port 8000
+    API_NOW_PLAYING_URL: "https://7168db2df5b1e7f05dca4eb2ebd4c947.loophole.site" + "/api/nowplaying", // port 80
+    API_RADIO_STREAM_URL: "https://78d4239ebe02ee944faa08502751bdb9.loophole.site" + "/radio.mp3" // port 8000
 };
 
 export default CONFIG;
