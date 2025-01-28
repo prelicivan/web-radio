3d30eb633952b2dd26ca6101fbb6a43b384a0ba6
Ivan Prelic <ivanprelicp@gmail.com>
2025-01-28 21:09:40 +0100

    url changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    API_NOW_PLAYING_URL: "172.232.206.170" + "/api/nowplaying", // port 80
-    API_RADIO_STREAM_URL: "172.232.206.170" + "/radio.mp3" // port 8000
+    API_NOW_PLAYING_URL: "https://172.232.206.170" + "/api/nowplaying", // port 80
+    API_RADIO_STREAM_URL: "https://172.232.206.170:8000" + "/radio.mp3" // port 8000
 };
 
 export default CONFIG;
