37add50347e038e1dbe15d322affb704dd4e1b28
Ivan Prelic <ivanprelicp@gmail.com>
2025-02-06 16:38:17 +0100

    added gallery and contact, gallery needs more work

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import './Contact.css'
+import { useOutletContext } from "react-router-dom";
 
-function Contact ({
-    songHistoryActive
-}) {
+function Contact () {
+    const { songHistory } = useOutletContext();
+
     return (
-        <div className="contact-component">
-            
+        <div className={`contact-component ${songHistory ? "song-history-active" : ""}`}>
+            <div className="left-side-container">
+                <div className="big-logo">
+                        <h3>RADIO</h3>
+                        <h1>DŽUNGLA</h1>
+                </div>
+                <div className="contact-info">
+                    <div className="email">
+                        Email: dzungla@radio.rs
+                    </div>
+                    <div className="address">
+                        Address: Beograd, Srbija
+                    </div>
+                    <div className="social-media">
+                        <a>Instagram</a>
+                        <a>Youtube</a>
+                        <a>Soundcloud</a>
+                    </div>
+                </div>
+            </div>
+            <div className="about-us">
+                <div className="title">About Us</div>
+                <div className="text">
+                    <p>
+                        Text text text text text text text text text text text text text text text text text text text text 
+                    </p>
+                    <p>
+                        Text text text text text text text text text text text text text text text text text text text text 
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -7,9 +7,28 @@ function Gallery () {
 
     return (
         <div className={`gallery-component ${songHistory ? "song-history-active" : ""}`}>
-            
+             <div className="album-viewer">
+              <div className="album-info">
+                <div className="album-name">Album Name</div>
+                <div className="album-description">
+                  Album description album description album description album description
+                  album description album description album description album description
+                  album description
+                </div>
+              </div>
+              <div className="album-photos">
+                {[...Array(9)].map((_, index) => (
+                  <div key={index} className="photo"></div>
+                ))}
+              </div>
+            </div>
+            <div className="album-search">
+              {[...Array(6)].map((_, index) => (
+                <div key={index} className="album">Album {index + 1}</div>
+              ))}
+            </div>
         </div>
-    )
+      );
 }
 
 export default Gallery;
